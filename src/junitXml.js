bee31010ab9e8286e74e313530cdb1f16eff5cd9
jcastello-hv <jcastello@healthverity.com>
2024-02-29 14:10:16 -0800

    test

--- src/junitXml.js
+++ src/junitXml.js
@@ -41,7 +41,7 @@ const getSummary = (data) => {
     core.warning(`JUnitXml file is not XML or not well-formed`);
     return '';
   }
-  if (parser.resultObject.testsuites['$'].keys.length === 0) {
+  if (parser.resultObject.testsuites.$.length === 0) {
     return parser.resultObject.testsuites.testsuite[0]['$'];
   } else {
     return parser.resultObject.testsuites['$'];
